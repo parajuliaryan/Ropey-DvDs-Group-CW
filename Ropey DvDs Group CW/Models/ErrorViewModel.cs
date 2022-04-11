39957486e856de5042c8001a4fd4257c22fb3f83
Aryan Parajuli <aryanparajuli007@gmail.com>
2022-04-11 09:03:59 +0545

    Add project files.

---
+++ Ropey DvDs Group CW/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ropey_DvDs_Group_CW.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
