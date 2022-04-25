2f5a293837ea2243163551d86cea321aaf4efe6e
Aryan Parajuli <aryanparajuli007@gmail.com>
2022-04-25 09:51:48 +0545

    migration updates

--- Ropey DvDs Group CW/DBContext/ApplicationDBContext.cs
+++ Ropey DvDs Group CW/DBContext/ApplicationDBContext.cs
@@ -17,29 +17,30 @@ namespace Ropey_DvDs_Group_CW.DBContext
         }
 
         /*
-        public DbSet<Ropey_DvDs_Group_CW.Models.UserRegisterModel> UserRegisterModel { get; set; }
+        public DbSet<UserRegisterModel> UserRegisterModel { get; set; }
 
-        public DbSet<Ropey_DvDs_Group_CW.Models.ActorModel> ActorModel { get; set; }
+        public DbSet<ActorModel> ActorModel { get; set; }
 
-        public DbSet<Ropey_DvDs_Group_CW.Models.StudioModel> StudioModel { get; set; }
+        public DbSet<StudioModel> StudioModel { get; set; }
 
-        public DbSet<Ropey_DvDs_Group_CW.Models.ProducerModel> ProducerModel { get; set; }
+        public DbSet<ProducerModel> ProducerModel { get; set; }
 
-        public DbSet<Ropey_DvDs_Group_CW.Models.LoanTypeModel> LoanTypeModel { get; set; }
+        public DbSet<LoanTypeModel> LoanTypeModel { get; set; }
 
-        public DbSet<Ropey_DvDs_Group_CW.Models.DVDCategoryModel> DVDCategoryModel { get; set; }
+        public DbSet<DVDCategoryModel> DVDCategoryModel { get; set; }
 
-        public DbSet<Ropey_DvDs_Group_CW.Models.MembershipCategoryModel> MembershipCategoryModel { get; set; }
+        public DbSet<MembershipCategoryModel> MembershipCategoryModel { get; set; }
 
-        public DbSet<Ropey_DvDs_Group_CW.Models.DVDTitleModel> DVDTitleModel { get; set; }
+        public DbSet<DVDTitleModel> DVDTitleModel { get; set; }
 
-        public DbSet<Ropey_DvDs_Group_CW.Models.DVDCopyModel> DVDCopyModel { get; set; }
+        public DbSet<DVDCopyModel> DVDCopyModel { get; set; }
 
-        public DbSet<Ropey_DvDs_Group_CW.Models.CastMemberModel> CastMemberModel { get; set; }
+        public DbSet<CastMemberModel> CastMemberModel { get; set; }
 
-        public DbSet<Ropey_DvDs_Group_CW.Models.MemberModel> MemberModel { get; set; }
+        public DbSet<MemberModel> MemberModel { get; set; }
 
-        public DbSet<Ropey_DvDs_Group_CW.Models.LoanModel> LoanModel { get; set; }
+        public DbSet<LoanModel> LoanModel { get; set; }
         */
+
     }
 }
